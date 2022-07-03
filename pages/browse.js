d2b88fe43a21e1fe160f3fc4a9bc9700604473e1
ryan0mor <ryanomor@gmail.com>
2022-07-03 15:17:19 +0200

    install drei and configure 3d object and materials host

--- pages/browse.js
+++ pages/browse.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar.js';
+import { Canvas } from '@react-three/fiber'
+
 
 const Browse = () => {
     return (
         <div>
             <Navbar></Navbar>
             <h1>About me</h1>
+            <Canvas>
+                <ambientLight intensity={0.1} />
+                <directionalLight color="red" position={[0, 0, 5]} />
+                <mesh>
+                    <boxGeometry args={[4, 4, 4]}/>
+                    <meshStandardMaterial />
+                </mesh>
+            </Canvas>
         </div>
     )
 }
--- pages/configurator.js
+++ pages/configurator.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Navbar from '../components/Configurator/Navbar/Navbar.js';
 import Actionbar from '../components/Actionbar/Actionbar.js';
 import Image from 'next/image';
-
+import { Canvas } from '@react-three/fiber'
+import { useGLTF, PresentationControls, Environment, ContactShadows, Html } from '@react-three/drei'
+import { useRef } from 'react'
 
 const Configurator = () => {
+    const [stadoMaterial, setEstadoMaterial] = React.useState("");
+
     return (
-        <div class="h-screen bg-slate-600">
+        <div class="h-screen bg-slate-200">
             <Navbar></Navbar>
             <div class="h-[calc(100%-50px)] flex-col flex lg:flex-row">
                 <div class="bg-slate-300 h-1/4 lg:h-[calc(100%-50px)] lg:basis-1/4">
@@ -32,10 +36,8 @@ const Configurator = () => {
                         </div> 
                     </div>                 
                 </div>
-                <div class="bg-slate-50 h-1/2 lg:h-full lg:basis-1/2">
-                    <div>
-                        <h1>3D viewer</h1>
-                    </div>
+                <div class="bg-slate-900 h-1/2 lg:h-full lg:basis-1/2">
+                    <Model></Model>
                 </div>
                 <div class="bg-slate-300 h-1/4 lg:h-[calc(100%-50px)] lg:basis-1/4">
                     <div class="h-[50px]">
@@ -43,20 +45,17 @@ const Configurator = () => {
                     </div>
                     <div class="h-[calc(100%-50px)] lg:h-full">
                         <div class="h-full flex flex-row lg:grid gap-2 p-2 lg:grid-cols-2 overflow-auto bg-slate-600">
-                            <div class="h-full aspect-square bg-slate-200">Material</div>
-                            <div class="h-full aspect-square bg-slate-200">Material</div>
-                            <div class="h-full aspect-square bg-slate-200">Material</div>
-                            <div class="h-full aspect-square bg-slate-200">Material</div>
-                            <div class="h-full aspect-square bg-slate-200">Material</div>
-                            <div class="h-full aspect-square bg-slate-200">Material</div>
-                            <div class="h-full aspect-square bg-slate-200">Material</div>
-                            <div class="h-full aspect-square bg-slate-200">Material</div>
-                            <div class="h-full aspect-square bg-slate-200">Material</div>
-                            <div class="h-full aspect-square bg-slate-200">Material</div>
-                            <div class="h-full aspect-square bg-slate-200">Material</div>
-                            <div class="h-full aspect-square bg-slate-200">Material</div>
-                            <div class="h-full aspect-square bg-slate-200">Material</div>
-                            <div class="h-full aspect-square bg-slate-200">Material</div>
+                            <MaterialItem></MaterialItem>
+                            <MaterialItem></MaterialItem>
+                            <MaterialItem></MaterialItem>
+                            <MaterialItem></MaterialItem>
+                            <MaterialItem></MaterialItem>
+                            <MaterialItem></MaterialItem>
+                            <MaterialItem></MaterialItem>
+                            <MaterialItem></MaterialItem>
+                            <MaterialItem></MaterialItem>
+                            <MaterialItem></MaterialItem>
+                            <MaterialItem></MaterialItem>
                         </div> 
                     </div>    
                 </div>
@@ -66,3 +65,52 @@ const Configurator = () => {
 }
 
 export default Configurator;
+
+    function MaterialItem() {
+        return (
+            <div class="h-full w-full aspect-square bg-slate-50">
+                <Canvas camera={{ position: [0, 0, 4], fov: 32}}>
+                    <ambientLight intensity={0.1} />
+                    <directionalLight color="white" position={[0, 0, 5]} />
+                    <mesh
+                        onClick={() => {
+                            console.log("clicked");
+                        }}
+                    >
+                        <sphereGeometry />
+                        <meshStandardMaterial />   
+                    </mesh>
+                </Canvas>
+            </div>
+        )
+    }
+
+    function Model() {
+        return (
+            <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
+                <ambientLight intensity={0.5} />
+                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} shadow-mapSize={[512, 512]} castShadow />
+                <PresentationControls
+                    global
+                    config={{ mass: 2 }}
+                    rotation={[0, 0, 0]}
+                    polar={[-Math.PI / 3, Math.PI / 3]}
+                    azimuth={[-Math.PI / 1.4, Math.PI / 2]}>
+                    <Watch rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0.25]} scale={0.003} />
+                </PresentationControls>
+                <ContactShadows position={[0, -1.4, 0]} opacity={0.75} scale={10} blur={2.5} far={4} />
+                <Environment preset="city" />
+            </Canvas>
+        )
+    }
+  
+  function Watch(props) {
+    const ref = useRef()
+    const { nodes, materials } = useGLTF('/watch-v1.glb')
+    return (
+      <group ref={ref} {...props} dispose={null}>
+        <mesh geometry={nodes.Object005_glass_0.geometry} material={materials.glass}></mesh>
+        <mesh castShadow receiveShadow geometry={nodes.Object006_watch_0.geometry} material={materials.watch} />
+      </group>
+    )
+  }
