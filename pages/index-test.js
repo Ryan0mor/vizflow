a72d80d8fe4c8442ebcc32387fcfefc137a70694
ryan0mor <ryanomor@gmail.com>
2022-08-05 11:33:11 +0200

    Add background

--- pages/index-test.js
+++ pages/index-test.js
@@ -35,10 +35,23 @@ const Index = () => {
     function IconTab(props) {
         return (
             <div className='h-24 w-24 bg-slate-200 flex flex-col justify-center items-center'>
-                <svg width="50" height="50" viewBox="0 0 24 24">
-                    <path d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z"></path>
-                </svg>
-                <h6>{props.categoryName}</h6>
+                <Link href='' >
+                    <a className='h-24 w-24 bg-slate-200 flex flex-col justify-center items-center active:text-indigo-500 text-slate-400 hover:text-slate-600'>
+                        <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg" className=''>
+                            <path d="M4 31H46V36C46 41.5228 41.5228 46 36 46H14C8.47715 46 4 41.5228 4 36V31Z" fill="currentColor" fill-opacity="0.25"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M44 33H6V36C6 40.4183 9.58172 44 14 44H36C40.4183 44 44 40.4183 44 36V33ZM4 31V36C4 41.5228 8.47715 46 14 46H36C41.5228 46 46 41.5228 46 36V31H4Z" fill="currentColor"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12 6C8.68629 6 6 8.68629 6 12V31H4V12C4 7.58172 7.58172 4 12 4H12.5C16.6421 4 20 7.35786 20 11.5H18C18 8.46243 15.5376 6 12.5 6H12Z" fill="currentColor"/>
+                            <path d="M26 19L12 19L12 18C12 14.134 15.134 11 19 11C22.866 11 26 14.134 26 18L26 19Z" fill="currentColor" fill-opacity="0.25"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M25.9291 17C25.9758 17.3266 26 17.6605 26 18L26 19L12 19L12 18C12 17.6605 12.0242 17.3266 12.0709 17C12.5561 13.6077 15.4735 11 19 11C22.5265 11 25.4439 13.6077 25.9291 17ZM14.1 17L23.9 17C23.4367 14.7178 21.419 13 19 13C16.581 13 14.5633 14.7178 14.1 17Z" fill="currentColor"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M13 27V21H15V27H13Z" fill="currentColor"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M4 33L2 33L2 31L4 31L4 33Z" fill="currentColor"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M48 33L46 33L46 31L48 31L48 33Z" fill="currentColor"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M23 27V21H25V27H23Z" fill="currentColor"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M18 27V21H20V27H18Z" fill="currentColor"/>
+                        </svg>
+                        <h6 className='whitespace-nowrap'>{props.categoryName}</h6>
+                    </a>
+                </Link>
             </div>
         )
     }
@@ -148,9 +161,9 @@ const Index = () => {
                 <div className='bg-slate-'></div>
             </div>
 
-            <div className='gradient-bg py-24'>
-                <div className='container mx-auto grid lg:grid-cols-12'>
-                    <div className='backdrop-filter backdrop-blur-sm bg-opacity-40 bg-slate-50 border rounded-2xl px-12 py-16 col-start-1 col-end-5 flex flex-col gap-4'>
+            <div className='py-36 bg-fixed bg-[url("https://pixabay.com/get/ga31db0d3f7bb2d4775628f8bfac72dc39f3094b4100be717d0960e15ecd3e230bd3dbb946a2201c4b9bf0c782f645984a316ca926c25caba6cbcdd4a9de11e8ad074ece218155e3b7ab3d8cd9938654f_1920.jpg")]'>
+                <div className='container mx-auto grid grid-cols-12'>
+                    <div className='backdrop-filter backdrop-blur-sm bg-opacity-40 bg-slate-50 border rounded-2xl px-12 py-16 col-start-1 md:col-end-8 lg:col-end-5 flex flex-col gap-4'>
                         <h5 className='header04'>Design</h5>
                         <h2 className='header01'>Create stunning 3d visualizations</h2>
                         <p className='paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
