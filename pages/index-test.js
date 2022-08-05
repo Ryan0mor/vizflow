1cc8b14b085ae8c714b5e3eb612aa6603060c991
ryan0mor <ryanomor@gmail.com>
2022-08-05 12:17:03 +0200

    Solve images missmatch warnings

--- pages/index-test.js
+++ pages/index-test.js
@@ -38,16 +38,16 @@ const Index = () => {
                 <Link href='' >
                     <a className='h-24 w-24 bg-slate-200 flex flex-col justify-center items-center active:text-indigo-500 text-slate-400 hover:text-slate-600'>
                         <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg" className=''>
-                            <path d="M4 31H46V36C46 41.5228 41.5228 46 36 46H14C8.47715 46 4 41.5228 4 36V31Z" fill="currentColor" fill-opacity="0.25"/>
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M44 33H6V36C6 40.4183 9.58172 44 14 44H36C40.4183 44 44 40.4183 44 36V33ZM4 31V36C4 41.5228 8.47715 46 14 46H36C41.5228 46 46 41.5228 46 36V31H4Z" fill="currentColor"/>
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12 6C8.68629 6 6 8.68629 6 12V31H4V12C4 7.58172 7.58172 4 12 4H12.5C16.6421 4 20 7.35786 20 11.5H18C18 8.46243 15.5376 6 12.5 6H12Z" fill="currentColor"/>
-                            <path d="M26 19L12 19L12 18C12 14.134 15.134 11 19 11C22.866 11 26 14.134 26 18L26 19Z" fill="currentColor" fill-opacity="0.25"/>
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M25.9291 17C25.9758 17.3266 26 17.6605 26 18L26 19L12 19L12 18C12 17.6605 12.0242 17.3266 12.0709 17C12.5561 13.6077 15.4735 11 19 11C22.5265 11 25.4439 13.6077 25.9291 17ZM14.1 17L23.9 17C23.4367 14.7178 21.419 13 19 13C16.581 13 14.5633 14.7178 14.1 17Z" fill="currentColor"/>
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M13 27V21H15V27H13Z" fill="currentColor"/>
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M4 33L2 33L2 31L4 31L4 33Z" fill="currentColor"/>
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M48 33L46 33L46 31L48 31L48 33Z" fill="currentColor"/>
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M23 27V21H25V27H23Z" fill="currentColor"/>
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M18 27V21H20V27H18Z" fill="currentColor"/>
+                            <path d="M4 31H46V36C46 41.5228 41.5228 46 36 46H14C8.47715 46 4 41.5228 4 36V31Z" fill="currentColor" fillOpacity="0.25"/>
+                            <path fillRule="evenodd" clipRule="evenodd" d="M44 33H6V36C6 40.4183 9.58172 44 14 44H36C40.4183 44 44 40.4183 44 36V33ZM4 31V36C4 41.5228 8.47715 46 14 46H36C41.5228 46 46 41.5228 46 36V31H4Z" fill="currentColor"/>
+                            <path fillRule="evenodd" clipRule="evenodd" d="M12 6C8.68629 6 6 8.68629 6 12V31H4V12C4 7.58172 7.58172 4 12 4H12.5C16.6421 4 20 7.35786 20 11.5H18C18 8.46243 15.5376 6 12.5 6H12Z" fill="currentColor"/>
+                            <path d="M26 19L12 19L12 18C12 14.134 15.134 11 19 11C22.866 11 26 14.134 26 18L26 19Z" fill="currentColor" fillOpacity="0.25"/>
+                            <path fillRule="evenodd" clipRule="evenodd" d="M25.9291 17C25.9758 17.3266 26 17.6605 26 18L26 19L12 19L12 18C12 17.6605 12.0242 17.3266 12.0709 17C12.5561 13.6077 15.4735 11 19 11C22.5265 11 25.4439 13.6077 25.9291 17ZM14.1 17L23.9 17C23.4367 14.7178 21.419 13 19 13C16.581 13 14.5633 14.7178 14.1 17Z" fill="currentColor"/>
+                            <path fillRule="evenodd" clipRule="evenodd" d="M13 27V21H15V27H13Z" fill="currentColor"/>
+                            <path fillRule="evenodd" clipRule="evenodd" d="M4 33L2 33L2 31L4 31L4 33Z" fill="currentColor"/>
+                            <path fillRule="evenodd" clipRule="evenodd" d="M48 33L46 33L46 31L48 31L48 33Z" fill="currentColor"/>
+                            <path fillRule="evenodd" clipRule="evenodd" d="M23 27V21H25V27H23Z" fill="currentColor"/>
+                            <path fillRule="evenodd" clipRule="evenodd" d="M18 27V21H20V27H18Z" fill="currentColor"/>
                         </svg>
                         <h6 className='whitespace-nowrap'>{props.categoryName}</h6>
                     </a>
@@ -138,22 +138,22 @@ const Index = () => {
                 </div>
                 <div className=' grid grid-cols-2 lg:grid-cols-4 gap-8'>
                     <IconCard 
-                        url='/../public/images/game-ready-icon-card.png'
+                        url='/images/game-ready-icon-card.png'
                         title='Game Ready' 
                         body='Fully optimized assets for realtime workflows.'
                     />
                     <IconCard 
-                        url='/../public/images/virtual-reality-ready-icon-card.png'
+                        url='/images/virtual-reality-ready-icon-card.png'
                         title='VR Ready' 
                         body='Fully optimized assets for realtime workflows.'
                     />
                     <IconCard 
-                        url='/../public/images/augmented-reality-ready-icon-card.png'
+                        url='/images/augmented-reality-ready-icon-card.png'
                         title='AR Ready' 
                         body='Create fluid lightweight augmented reality solutions.'
                     />
                     <IconCard 
-                        url='/../public/images/web-ready-icon-card.png'
+                        url='/images/web-ready-icon-card.png'
                         title='Web Ready' 
                         body='Easily bring 3d experiences to web browsers.'
                     />
@@ -180,42 +180,42 @@ const Index = () => {
 
                     <div className='grid lg:grid-cols-4 gap-4'>
                         <IconCard 
-                            url='/../public/images/game-ready-icon-card.png'
+                            url='/images/game-ready-icon-card.png'
                             title='Game Ready' 
                             body='Fully optimized assets for realtime workflows.'
                         />
                         <IconCard 
-                            url='/../public/images/virtual-reality-ready-icon-card.png'
+                            url='/images/virtual-reality-ready-icon-card.png'
                             title='VR Ready' 
                             body='Fully optimized assets for realtime workflows.'
                         />
                         <IconCard 
-                            url='/../public/images/augmented-reality-ready-icon-card.png'
+                            url='/images/augmented-reality-ready-icon-card.png'
                             title='AR Ready' 
                             body='Create fluid lightweight augmented reality solutions.'
                         />
                         <IconCard 
-                            url='/../public/images/web-ready-icon-card.png'
+                            url='/images/web-ready-icon-card.png'
                             title='Web Ready' 
                             body='Easily bring 3d experiences to web browsers.'
                         />
                         <IconCard 
-                            url='/../public/images/game-ready-icon-card.png'
+                            url='/game-ready-icon-card.png'
                             title='Game Ready' 
                             body='Fully optimized assets for realtime workflows.'
                         />
                         <IconCard 
-                            url='/../public/images/virtual-reality-ready-icon-card.png'
+                            url='/images/virtual-reality-ready-icon-card.png'
                             title='VR Ready' 
                             body='Fully optimized assets for realtime workflows.'
                         />
                         <IconCard 
-                            url='/../public/images/augmented-reality-ready-icon-card.png'
+                            url='/images/augmented-reality-ready-icon-card.png'
                             title='AR Ready' 
                             body='Create fluid lightweight augmented reality solutions.'
                         />
                         <IconCard 
-                            url='/../public/images/web-ready-icon-card.png'
+                            url='/images/web-ready-icon-card.png'
                             title='Web Ready' 
                             body='Easily bring 3d experiences to web browsers.'
                         />
@@ -242,32 +242,32 @@ const Index = () => {
                     </div>
                     <div className='grid lg:grid-cols-4 gap-4'>
                         <CategoryCard 
-                            image='/../public/images/web-ready-icon-card.png'
+                            image='/images/web-ready-icon-card.png'
                             title='Chairs'
                             url='/#'  
                         />
                         <CategoryCard 
-                            image='/../public/images/web-ready-icon-card.png'
+                            image='/images/web-ready-icon-card.png'
                             title='Chairs'
                             url='/#'  
                         />
                         <CategoryCard 
-                            image='/../public/images/web-ready-icon-card.png'
+                            image='/images/web-ready-icon-card.png'
                             title='Chairs'
                             url='/#'  
                         />
                         <CategoryCard 
-                            image='/../public/images/web-ready-icon-card.png'
+                            image='/images/web-ready-icon-card.png'
                             title='Chairs'
                             url='/#'  
                         />
                         <CategoryCard 
-                            image='/../public/images/web-ready-icon-card.png'
+                            image='/images/web-ready-icon-card.png'
                             title='Chairs'
                             url='/#'  
                         />
                         <CategoryCard 
-                            image='/../public/images/web-ready-icon-card.png'
+                            image='/images/web-ready-icon-card.png'
                             title='Chairs'
                             url='/#'  
                         />
