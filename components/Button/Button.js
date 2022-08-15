2f7241f2315faa4e6a499e445de123d9d246bdfa
ryan0mor <ryanomor@gmail.com>
2022-08-15 09:16:32 +0200

    Style buttons

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -8,7 +8,7 @@ const Button = (props) => {
         case "1":
             return (
                 <Link href={props.link}>
-                    <button className='bg-teal-400 hover:bg-teal-300 header06 shadow-lg shadow-teal-400/50 text-white px-5 py-2.5 rounded-xl'>
+                    <button className='bg-teal-400 hover:bg-teal-300 header06 shadow-lg shadow-teal-400/50 text-white px-5 py-2.5 rounded-xl' type="button">
                         {props.content}
                     </button>
                 </Link>
@@ -16,7 +16,7 @@ const Button = (props) => {
         case "2":
             return (
                 <Link href={props.link}>
-                    <button className=' header06 bg-purple-400 hover:bg-purple-300 shadow-lg shadow-purple-400/50 text-white px-5 py-2.5 rounded-xl'>
+                    <button className=' header06 bg-purple-400 hover:bg-purple-300 shadow-lg shadow-purple-400/50 text-white px-5 py-2.5 rounded-xl' type="button">
                         {props.content}
                     </button>
                 </Link>
@@ -24,7 +24,7 @@ const Button = (props) => {
         case "3":
             return (
                 <Link href={props.link}>
-                    <button className=' header06 bg-pink-400 hover:bg-pink-300 shadow-lg shadow-pink-400/50 text-white px-5 py-2.5 rounded-xl'>
+                    <button className=' header06 bg-pink-400 hover:bg-pink-300 shadow-lg shadow-pink-400/50 text-white px-5 py-2.5 rounded-xl' type="button">
                         {props.content}
                     </button>
                 </Link>
@@ -32,7 +32,7 @@ const Button = (props) => {
         case "4":
             return (
                 <Link href={props.link}>
-                    <button className=' header06 bg-blue-400 hover:bg-blue-300 shadow-lg shadow-blue-400/50 text-white px-5 py-2.5 rounded-xl'>
+                    <button className=' header06 bg-blue-400 hover:bg-blue-300 shadow-lg shadow-blue-400/50 text-white px-5 py-2.5 rounded-xl' type="button">
                         {props.content}
                     </button>
                 </Link>
@@ -40,7 +40,7 @@ const Button = (props) => {
         default:
             return (
                 <Link href={props.link}>
-                    <button className=' paragraphReg bg-indigo-500 shadow-lg shadow-indigo-500/50 text-white px-6 py-3 rounded-xl'>
+                    <button className=' paragraphReg bg-indigo-500 shadow-lg shadow-indigo-500/50 text-white px-6 py-3 rounded-xl' type="button">
                         {props.content}
                     </button>
                 </Link>
