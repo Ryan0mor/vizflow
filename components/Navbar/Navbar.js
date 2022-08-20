c1a9324218dbec5319873b052bd0155c2a83ae98
ryan0mor <ryanomor@gmail.com>
2022-08-20 14:02:33 +0200

    Make navbar responsive

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,78 +1,113 @@
-import React from 'react'
-
 import Button from '../../components/Button/Button.js'
 import Logo from '../../components/Logo/Logo.js'
-
 import Link from 'next/link'
+import { Menu } from '@mantine/core';
 
-const Navbar = () => {
+export default function Navbar () {
+  const linkData = [
+    {
+      id: 2,
+      name: "Browse",
+      link: "/browse",
+    },
+    {
+      id: 3,
+      name: "Integrations",
+      link: "/#integrations",
+    },
+    {
+      id: 4,
+      name: "About",
+      link: "/about",
+    },
+    {
+      id: 5,
+      name: "Contact",
+      link: "/contact",
+    }   
+  ];
 
-    const linkData = [
-        {
-            id: 2,
-            name: "Assets",
-            link: "/assets",
-        },
-        {
-            id: 3,
-            name: "Integrations",
-            link: "/#integrations",
-        },
-        {
-            id: 4,
-            name: "About",
-            link: "/about",
-        },
-        {
-            id: 5,
-            name: "Contact",
-            link: "contact",
-        }   
-    ];
-
-    function createLink(link) {
-        return <LinkItem
-            key={link.id}
-            name={link.name}
-            link={link.link}
-        />
-    }
-
-    function LinkItem(props) {
-        return (
-            <Link href={props.link }>
-                <a className='hover:text-teal-400 header06 '>
-                    <div className=''>
-                        {props.name}
-                    </div>
-                </a>
-            </Link>
-        )
-    }
-
+  function LinkItemDesktop(props) {
     return (
-        <nav className='bg-white border-b border-slate-200 py-3'>
-            <div className='container flex flex-wrap justify-between items-center mx-auto'>
-                <Logo />
-                <div className='flex flex-row gap-8'>
-                    {linkData.map(createLink)}
-                </div>
-                
-                <div className='flex gap-6'>
-                    <Button 
-                        content='Log in' 
-                        link='#'
-                        choice='1'/>
-                    <Button 
-                        content='Get started' 
-                        link='#'
-                        choice='2'/>
-                </div>
-
-            </div>
-            
-        </nav>
+      <Link href={props.link }>
+        <a className='hover:text-teal-400 header06 '>
+          <div className=''>
+            {props.name}
+          </div>
+        </a>
+      </Link>
     )
-}
+  }
 
-export default Navbar
+  function LinkItemMobile(props) {
+    return (
+      <Menu.Item>
+        <Link href={props.link }>
+          <a className='hover:text-teal-400 header06 '>
+            <div className=''>
+              {props.name}
+            </div>
+          </a>
+        </Link>
+      </Menu.Item>
+    )
+  }
+
+  return (
+    <nav className='bg-white border-b border-slate-200 py-3 px-4 md:px-0'>
+      <div className='container flex flex-wrap justify-between items-center mx-auto'>
+        <Logo />
+        <div className='hidden md:flex flex-row gap-8'>
+          {linkData.map((link) => (
+            <LinkItemDesktop
+              key={link.id}
+              name={link.name}
+              link={link.link}
+              type='desktop'
+            />
+          ))}
+        </div>
+        
+        <div className='hidden md:flex gap-6'>
+          <Button 
+            content='Log in' 
+            link='#'
+            choice='1'/>
+          <Button 
+            content='Get started' 
+            link='#'
+            choice='2'/>
+        </div>
+        <Menu className='block md:hidden'>
+          <Menu.Target>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+          </Menu.Target>
+          <Menu.Dropdown className='w-3/4 p-5 rounded-2xl'>
+            {linkData.map((link) => (
+              <LinkItemMobile
+                key={link.id}
+                name={link.name}
+                link={link.link}
+              />
+            ))}
+            <Menu.Divider />
+            <Menu.Item>
+              <Button 
+                content='Log in' 
+                link='#'
+                choice='1'/>
+            </Menu.Item>
+            <Menu.Item>
+              <Button 
+                content='Get started' 
+                link='#'
+                choice='2'/>
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+      </div> 
+    </nav>
+  )
+}
